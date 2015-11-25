ea66c49874bfed52a5976592af90e49e6233c94c
phantomk <540444533@qq.com>
2015-11-25 23:09:26 +0800

    commit

---
+++ hotel_manage/README.md
@@ -0,0 +1,9 @@
+# hotel_manage
+
+- 学期实践项目-酒店管理系统
+- 基于flat.ui
+- 图标采用svg
+- web弹窗插件: layer
+
+
+
---
+++ hotel_manage/js/index.js
@@ -0,0 +1,11 @@
+// TODO: 弹出框
+/**
+ * 弹出框
+ */
+
+
+
+/**
+ * 天气：调用百度天气api
+ */
+var weather_json = "http://api.map.baidu.com/telematics/v3/weather?location=%E6%9D%AD%E5%B7%9E&output=json&ak=U1fGe9lfreG6OzmxSWv237Gf";
