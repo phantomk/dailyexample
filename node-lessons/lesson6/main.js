c366b06c4fa3c401fa195cb156cc35c7b269a619
phantomk <phantomk@foxmail.com>
2015-12-17 23:46:42 +0800

    update

--- node-lessons/lesson6/main.js
+++ node-lessons/lesson6/main.js
@@ -6,7 +6,7 @@ var fibonacci = function (n) {
         throw new Error('n should be a Number');
     }
     if (n < 0) {
-        throw new Error('n should >= 0')
+        throw new Error('n should >= 0');
     }
     if (n > 10) {
         throw new Error('n should <= 10');
